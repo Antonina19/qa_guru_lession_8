2b9b6881e25b01615e097d76140706db9ffdf2d1
tony <tony-94@bk.ru>
2021-07-23 17:10:22 +0300

    changed text

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
     @Test
     void firstTest(){
-        System.out.println("First Test!!!");
+        System.out.println("firstTest!!!");
         Selenide.open("http://ya.ru ");
 
     }
